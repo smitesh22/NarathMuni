e18be35bea1c290e47c13acd3d851e4eb4acf81b
Smitesh Patil <smitesh22@gmail.com>
2024-09-21 01:41:53 +0100

    new commit

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.use(
-    require("../components/status/status.routes")
-);
+router.use(require("../components/status/status.routes"));
 
-router.use(
-    require("../components/uuid-generator/uuid-generator.routes")
-);
+router.use(require("../components/uuid-generator/uuid-generator.routes"));
 module.exports = router;
--- run-local-index.js
+++ run-local-index.js
@@ -1,5 +1,5 @@
-const app = require("./app/server")
-const port = 4000
+const app = require("./app/server");
+const port = 4000;
 app.listen(4000, () => {
-    console.info(`App is running on port http://localhost:${port}/`);
+  console.info(`App is running on port http://localhost:${port}/`);
 });
