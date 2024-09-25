30855c3954a254b7e1f6ee5c15899701e34ddedc
Smitesh Patil <smitesh22@gmail.com>
2024-09-25 21:32:52 +0100

    tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,17 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+# Define the Lambda function name
+locals {
+  lambda_function_name = "express-lambda"
+}
+
+# Check if the Lambda function exists
+data "aws_lambda_function" "existing_lambda" {
+  function_name = local.lambda_function_name
+}
+
+# S3 bucket for Lambda deployment
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = "narath-muni-api-bucket-v2"
 }
@@ -12,6 +23,7 @@ resource "aws_s3_object" "lambda_zip" {
   source = "../app.zip"
 }
 
+# IAM Role for Lambda execution
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role"
 
@@ -32,8 +44,11 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+# Create or update the Lambda function
 resource "aws_lambda_function" "express_lambda" {
-  function_name = "express-lambda"
+  count = data.aws_lambda_function.existing_lambda ? 0 : 1
+
+  function_name = local.lambda_function_name
   s3_bucket     = aws_s3_bucket.lambda_bucket.bucket
   s3_key        = aws_s3_object.lambda_zip.key
   handler       = "handler.handler"  # Adjust if necessary
@@ -41,6 +56,15 @@ resource "aws_lambda_function" "express_lambda" {
   role          = aws_iam_role.lambda_exec_role.arn
   memory_size   = 512
   timeout       = 10
+}
+
+# Update the existing Lambda function code if it exists
+resource "null_resource" "update_lambda_code" {
+  count = data.aws_lambda_function.existing_lambda ? 1 : 0
+
+  provisioner "local-exec" {
+    command = "aws lambda update-function-code --function-name ${local.lambda_function_name} --s3-bucket ${aws_s3_bucket.lambda_bucket.bucket} --s3-key ${aws_s3_object.lambda_zip.key} --region eu-west-1"
+  }
 }
 
 resource "aws_apigatewayv2_api" "http_api" {
@@ -51,7 +75,7 @@ resource "aws_apigatewayv2_api" "http_api" {
 resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id               = aws_apigatewayv2_api.http_api.id
   integration_type     = "AWS_PROXY"
-  integration_uri      = aws_lambda_function.express_lambda.arn
+  integration_uri      = aws_lambda_function.express_lambda[0].arn
   payload_format_version = "2.0"
 }
 
