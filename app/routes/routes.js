e7d003a6b7c182ebab7ded8bc7731402de55062d
Smitesh Patil <smitesh22@gmail.com>
2024-09-28 14:58:02 +0100

    main

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.use(require("../components/status/status.routes"));
-
 router.use(require("../components/uuid-generator/uuid-generator.routes"));
+
 module.exports = router;
