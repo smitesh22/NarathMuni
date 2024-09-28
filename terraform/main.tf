286112c1c911f33743a7eb11c697cb3987bfec9f
Smitesh Patil <smitesh22@gmail.com>
2024-09-28 14:26:33 +0100

    new

--- terraform/main.tf
+++ terraform/main.tf
@@ -100,16 +100,53 @@ resource "aws_lambda_function" "my_lambda_function" {
   }
 }
 
+# Declare the API Gateway resource
 resource "aws_api_gateway_rest_api" "my_api" {
   name = "Narath-Muni_API"
 }
 
-resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = [aws_api_gateway_integration.proxy_lambda_integration]
+# Define the proxy resource for API Gateway
+resource "aws_api_gateway_resource" "proxy_resource" {
   rest_api_id = aws_api_gateway_rest_api.my_api.id
-  stage_name  = "prod"
+  parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
+  path_part   = "{proxy+}"
 }
 
+# Define the API Gateway method for the proxy resource
+resource "aws_api_gateway_method" "proxy_any" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.proxy_resource.id
+  http_method = "ANY"
+  authorization = "NONE"
+}
+
+# Define the API Gateway integration for Lambda
+resource "aws_api_gateway_integration" "proxy_lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.my_api.id
+  resource_id             = aws_api_gateway_resource.proxy_resource.id
+  http_method             = aws_api_gateway_method.proxy_any.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.my_lambda_function.invoke_arn
+}
+
+# Allow API Gateway to invoke Lambda function
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "AllowAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.my_lambda_function.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.my_api.execution_arn}/*"
+}
+
+# API Gateway deployment
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on   = [aws_api_gateway_integration.proxy_lambda_integration]
+  rest_api_id  = aws_api_gateway_rest_api.my_api.id
+  stage_name   = "prod"
+}
+
+# Outputs
 output "api_gateway_url" {
   value = "${aws_api_gateway_rest_api.my_api.execution_arn}/prod/"
 }
