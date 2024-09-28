1c0868406349f091576e5b636d4bef8310f25bd0
Smitesh Patil <smitesh22@gmail.com>
2024-09-28 11:25:45 +0100

    new

--- app/components/status/status.js
+++ app/components/status/status.js
@@ -2,6 +2,11 @@ module.exports = async function (req, res, next) {
   try {
     res.send(`Everything is OK on ${process.env.ENV}`);
     console.log("ok");
+    //#endregion
+
+
+
+    console.log("aaa");
   } catch (error) {
     console.error("Error occured on GET/ Status");
   }
