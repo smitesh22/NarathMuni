5de78b7664fccc367cabfb555c1bf4b8000d2e49
Smitesh Patil <smitesh22@gmail.com>
2024-09-28 18:41:08 +0100

    new

--- app/components/status/status.js
+++ app/components/status/status.js
@@ -1,6 +1,6 @@
 module.exports = async function (req, res, next) {
   try {
-    res.send(`Smitesh Loves Aditi Very Very Much :)`);
+    res.send(`Smitesh Loves Aditi Very Very Much!!! :)`);
   } catch (error) {
     console.error("Error occured on GET/ Status");
   }
