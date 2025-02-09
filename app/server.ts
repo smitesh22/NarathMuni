53de2e4631532f4c527dcd4fbd94a25d9dc2ac06
Smitesh Patil <smitesh22@gmail.com>
2025-02-09 21:32:19 +0000

    async

--- app/server.ts
+++ app/server.ts
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 app.use(cors());
 app.use(passport.initialize());
 app.use("/", routes);
-app.get("/", (req: express.Request, res: express.Response, next: express.NextFunction,) => {
+app.get("/", async (req: express.Request, res: express.Response, next: express.NextFunction,) => {
   res.status(200).send(`Hello fom ${ENV} environment`);
 });
 
