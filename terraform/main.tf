c624631a6d7b7d215ccdbf299cc48df8ea6da32e
Smitesh Patil <smitesh22@gmail.com>
2025-02-09 18:17:29 +0000

    new dep

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,7 +91,7 @@ resource "aws_lambda_function" "my_lambda_function" {
   runtime          = "nodejs20.x"
   s3_bucket        = data.aws_s3_bucket.existing_bucket.bucket
   s3_key           = aws_s3_object.app_zip.key
-  source_code_hash = aws_s3_object.app_zip.etag
+  source_code_hash = filebase64sha256("../app.zip")
 
   environment {
     variables = {
