f56db6a5d7ca4afad8b922dc11fdb1652029ac0a
Smitesh Patil <smitesh22@gmail.com>
2025-02-09 16:48:45 +0000

    build

---
+++ build.js
@@ -0,0 +1,17 @@
+const esbuild = require('esbuild');
+
+esbuild.build({
+    entryPoints: ['./index.js'],
+    bundle: true,
+    platform: 'node',
+    target: 'node20',
+    outfile: './dist/index.js',
+    external: ['@prisma/client', 'aws-sdk'],
+    minify: true,
+    sourcemap: false,
+}).then(() => {
+    console.log('Build completed successfully for Node.js 20!');
+}).catch((err) => {
+    console.error('Build failed:', err);
+    process.exit(1);
+});
