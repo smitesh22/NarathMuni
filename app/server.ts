3dd6166a6d66288f2066ade58a8c0f6186ccbc89
Smitesh Patil <smitesh22@gmail.com>
2025-02-09 21:41:06 +0000

    add check endpoint

--- app/server.ts
+++ app/server.ts
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 app.use(cors());
 app.use(passport.initialize());
 app.use("/", routes);
-app.get("/", async (req: express.Request, res: express.Response, next: express.NextFunction,) => {
+app.get("/check", async (req: express.Request, res: express.Response, next: express.NextFunction,) => {
   res.status(200).send(`Hello fom ${ENV} environment`);
 });
 
