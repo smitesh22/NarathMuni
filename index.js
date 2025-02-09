3f7c550a97eeb89576da8af2a84f8de790966223
Smitesh Patil <smitesh22@gmail.com>
2025-02-09 18:58:12 +0000

    console.log

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const serverless = require("serverless-http");
 const app = require("./app/server");
 require("dotenv").config();
+console.log("App instance type:", typeof app);  // Should log "function" for an Express app
 module.exports.handler = serverless(app);
