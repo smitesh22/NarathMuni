b1b790df57ab0ece7b4755dc8db02ea27b5c36e6
Smitesh Patil <smitesh22@gmail.com>
2025-02-16 19:13:14 +0000

    opt template

--- app/helpers/user.helpers.ts
+++ app/helpers/user.helpers.ts
@@ -32,19 +32,28 @@ const transporter = nodemailer.createTransport({
 });
 
 const otpEmailTemplate = (otp: string, firstName: string) => `
-  <div style="font-family: Arial, sans-serif; line-height: 1.6;">
-    <div style="text-align: left; margin-bottom: 20px;">
-    <img src="https://dev.ledgefast.com/logo.png" alt="LedgeFast Logo" style="max-width: 150px; height: auto;">
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OTP Verification</title>
+  </head>
+  <body style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
+    <div style="margin-bottom: 20px; text-align: left;">
+      <img src="https://dev.ledgefast.com/logo.png" alt="LedgeFast Logo" style="max-width: 150px; height: auto;">
     </div>
     <p>Hello ${firstName},</p>
-    <p>${otp} is your one-time password (OTP) for the LedgeFast app.</p>
+    <p><strong>${otp}</strong> is your one-time password (OTP) for the LedgeFast app.</p>
     <p>Please use this code to complete your action. If it doesn't work, you can manually enter the code when prompted in the app.</p>
-    <p>The code was requested from the LedgFast website. It will expire in 5 minutes.</p>
+    <p>The code was requested from the LedgeFast website. It will expire in <strong>5 minutes</strong>.</p>
     <p>If you did not request this code, please disregard this email or contact LedgeFast support immediately for assistance.</p>
     <p>Enjoy using LedgeFast!</p>
     <p>Best regards,<br>The LedgeFast Team</p>
-  </div>
+  </body>
+  </html>
 `;
+
 
 export { createExtensions, transporter, otpEmailTemplate };
 
