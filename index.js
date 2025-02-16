6287dc34c3db5adcec60a8672c81aa6aff1d216c
Smitesh Patil <smitesh22@gmail.com>
2025-02-16 23:45:18 +0000

    new

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+import 'source-map-support/register';
+
 const serverless = require("serverless-http");
 const app = require("./app/server").default || require("./app/server");
+
+// Custom error logging
+process.on("uncaughtException", (err) => {
+    console.error("❌ Uncaught Exception:", err);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+    console.error("❌ Unhandled Promise Rejection at:", promise, "reason:", reason);
+});
+
 require("dotenv").config();
 console.log("App instance type:", typeof app);  // Should log "function" for an Express app
 module.exports.handler = serverless(app);
