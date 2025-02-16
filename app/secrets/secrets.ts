65c95320c410af4c67da5b5cc9caafa10845aba3
Smitesh Patil <smitesh22@gmail.com>
2025-02-16 19:05:27 +0000

    new

--- app/secrets/secrets.ts
+++ app/secrets/secrets.ts
@@ -1,8 +1,8 @@
 export const JWT_SECRET = process.env.JWT_SECRET as string;
 export const ENV = process.env.ENV as string;
-export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID as string;
+export const AWS_ACCESS_KEY_ID = process.env.ACCESS_KEY_ID as string;
 export const AWS_SECRET_ACCESS_KEY = process.env
-  .AWS_SECRET_ACCESS_KEY as string;
+  .SECRET_ACCESS_KEY as string;
 export const DATABASE_URL = process.env.DATABASE_URL as string;
 export const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME as string;
 export const S3_CLOUDFRONT_NAME = process.env.S3_CLOUDFRONT_NAME as string;
