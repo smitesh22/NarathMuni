4c3cf375d12a0160c7155a99bda3821991b1971b
Smitesh Patil <smitesh22@gmail.com>
2025-03-02 20:44:18 +0000

    deploy terrform

--- terraform/main.tf
+++ terraform/main.tf
@@ -136,15 +136,19 @@ resource "aws_api_gateway_integration" "proxy_lambda_integration" {
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = [aws_api_gateway_integration.proxy_lambda_integration]
   rest_api_id = aws_api_gateway_rest_api.my_api.id
-  stage_name  = "ignored"
+  triggers = {
+    redeployment = timestamp()
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_api_gateway_stage" "prod" {
-  deployment_id = aws_api_gateway_deployment.deployment.id
-  rest_api_id   = aws_api_gateway_rest_api.my_api.id
   stage_name    = "prod"
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
 }
 
 resource "aws_lambda_permission" "allow_api_gateway" {
