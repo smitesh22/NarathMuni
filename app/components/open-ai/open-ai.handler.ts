16ee0817dee0d36ccb11420a214b4ae4529ed276
Smitesh Patil <smitesh22@gmail.com>
2025-03-02 15:13:16 +0000

    dont send as base 64

--- app/components/open-ai/open-ai.handler.ts
+++ app/components/open-ai/open-ai.handler.ts
@@ -58,7 +58,7 @@ export default async function handler(
                 "Content-Type",
                 "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             );
-            res.send(workbookBuffer.toString('base64'));
+            res.send(workbookBuffer);
           } else {
             res.status(404).send({ message: "Image URL does not exist" });
           }
