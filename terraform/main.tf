fab8d77e56cfaef88106947d3065c43e35e27bd8
Smitesh Patil <smitesh22@gmail.com>
2025-03-02 21:07:07 +0000

    update process image

--- terraform/main.tf
+++ terraform/main.tf
@@ -138,6 +138,7 @@ resource "aws_api_gateway_integration" "proxy_lambda_integration" {
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [aws_api_gateway_integration.proxy_lambda_integration]
   rest_api_id = aws_api_gateway_rest_api.my_api.id
+  stage_name  = "prod"
 }
 
 resource "aws_lambda_permission" "allow_api_gateway" {
